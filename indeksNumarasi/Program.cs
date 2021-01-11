6f1a93c68eb36d37ce749f16373d4b5ec73108af
MsCurly <zuleyha_98@live.com>
2021-01-11 14:06:59 +0300

    ilk gonderme

---
+++ indeksNumarasi/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indeksNumarasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            string[] sehirler = { "Eskisehir", "Istanbul", "Ankara", "Izmir", "Diyarbakir", "Sinop" };
+            Console.WriteLine("Bir şehir giriniz:");
+            string girilenSehir = Console.ReadLine();
+            bool varMi = false;
+
+            for (i = 0; i < sehirler.Length+1; i++)
+            {
+                if (girilenSehir == sehirler[i])
+                {
+                    varMi = true;
+                    break;
+                }
+                    
+                else
+                    continue;
+            }
+            if (varMi)
+            {
+                Console.WriteLine($"{girilenSehir} array'de var ve indeks numarası {i+1}.");
+            }
+            else
+            {
+                Console.WriteLine($"{girilenSehir} array'de yok.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
